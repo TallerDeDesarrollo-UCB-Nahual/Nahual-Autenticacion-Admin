3e0b4e9f492bda40efc657cc91a632789aa86acb
Luis Toro <luis.toro.flores97@gmail.com>
2020-10-21 20:05:49 -0400

    redirect to others microservices

--- src/components/solicitudAcceso/FormularioDeSolicitud.js
+++ src/components/solicitudAcceso/FormularioDeSolicitud.js
@@ -3,7 +3,7 @@ import { Form, Input, TextArea, Button } from 'semantic-ui-react'
 import '../../public/Stylesheets/FormularioDeSolicitud.css'
 
 const FormExampleFieldControlId = () => (
-    <Form>
+    <Form >
         <Form.Group widths='equal'>
             <Form.Field
                 id='form-input-control-first-name'
@@ -22,18 +22,18 @@ const FormExampleFieldControlId = () => (
                 pointing: 'below',
             }}
         />
-
         <Form.Field
             id='form-textarea-control-opinion'
             control={TextArea}
             label='Motivo'
             placeholder='Motivo'
         />
-
         <Form.Field
             className="boton_confirm"
             id='form-button-control-public'
-            control={Button}
+            control={Button }
+            // onClick={() => window.location = "https://nahual-8298d.web.app/"}
+            onClick={() => window.location = "http://localhost:3000/"}
             content='Enviar'
         />
     </Form>
