27a41987f9c44f470d48c4e7ebce491990f8b3fd
unknown <pablo.canelas.schlink@gmail.com>
2020-11-09 20:10:29 -0400

    Se implementaron funciones para recuperar datos del form de acceso

--- src/components/otorgar-acceso/FormAcceso.js
+++ src/components/otorgar-acceso/FormAcceso.js
@@ -9,23 +9,45 @@ class FormAcceso extends Component{
     this.state = {
         nombre: "",
         correo: "",
+        aplicacion:"",
         exito: null
     }
- }
+  }
+
+  cambioEnInput = (e, {value, name}) => {
+    let valor = this.state[e.target.name];
+    let nombre = e.target.name;
+    this.setState({[nombre]: value})
+    valor = value;
+    this.state[e.target.name] = valor;
+  }
+
+  enConfirmacion = (evento) => {
+      evento.preventDefault();
+      var estadoDepurado = { 
+          nombre: this.state.nombre,
+          email: this.state.correo,
+          aplicacion: this.state.aplicacion
+      }
+      console.log(estadoDepurado);
+  }
   render() {
     return (
-      <Form id="myForm">
+      <Form id="myForm" onSubmit={this.enConfirmacion}>
         <Input 
           name = "nombre" 
+          value = {this.state.nombre}
           validators={['required','matchRegexp:^[A-Za-z ]+$']} 
           errorMessages={['Este campo es requerido', 'El campo no acepta valores numéricos']} 
           id='form-acceso'
           label='Nombre completo'
           placeholder='Nombre completo'
           width={16} 
+          onChange={this.cambioEnInput}
         />
         <Input
           name="correo"
+          value = {this.state.correo}
           type="email"
           id='form-input-control-error-email'
           label='Correo'
@@ -33,8 +55,9 @@ class FormAcceso extends Component{
           validators={['required']} 
           errorMessages={['Este campo es requerido']} 
           width={16}
+          onChange={this.cambioEnInput}
         />
-        <Button floated='right' type='submit'>Confirmar</Button>
+        <Button floated='right' type='submit' onSubmit={this.enConfirmacion}>Otorgar Acceso</Button>
       </Form>
     )
   }
