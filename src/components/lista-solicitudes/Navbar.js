989c6d1b8e313fdf84b4dabcab491745c7eb8b5d
melissafg <melissa.flores.g@ucb.edu.bo>
2020-11-10 19:30:45 -0400

    se arregló un import

--- src/components/lista-solicitudes/Navbar.js
+++ src/components/lista-solicitudes/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../../public/imagenes/logo.png';
-import '../../public/stylesheets/Navbar.css';
+import '../../public/Stylesheets/Navbar.css';
 
 export default class Navbar extends Component {
 
