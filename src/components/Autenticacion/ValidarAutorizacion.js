456e0ffdbcf862b6640843601d57a8cf4b414551
johancarlos <johancarlos.bo@gmail.com>
2020-11-12 00:19:58 -0400

    Se agrego la pagina de No acceso, y el boton de cerrar sesion

--- src/components/Autenticacion/ValidarAutorizacion.js
+++ src/components/Autenticacion/ValidarAutorizacion.js
@@ -3,64 +3,65 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Dimmer, Loader } from "semantic-ui-react";
 import Axios from "axios";
 import NahualTable from "../lista-solicitudes/Tabla";
+import NoAutorizado from "./NoAutorizado.js";
 
 const ValidarAutorizacion = () => {
-	const { user: usuario } = useAuth0();
-	const [state, setState] = useState({
-		validado: false,
-		mostrarIconoCargando: true
-	});
-	useEffect(() => {
-		verificarAutotizacion();
-		// eslint-disable-next-line
-	}, []);
-	const verificarAutotizacion = () => {
-		const SERVICIO_DE_VERIFICACION_API_SERVICIO_DE_DATOS =
-			process.env.REACT_APP_API_ACCESO_URL;
-		const datos = JSON.stringify({
-			nombre: usuario.name,
-			email: usuario.email,
-			aplicacion: "Admin"
+  const { user: usuario } = useAuth0();
+  const [state, setState] = useState({
+    validado: false,
+    mostrarIconoCargando: true
+  });
+  useEffect(() => {
+    verificarAutotizacion();
+    // eslint-disable-next-line
+  }, []);
+  const verificarAutotizacion = () => {
+    const SERVICIO_DE_VERIFICACION_API_SERVICIO_DE_DATOS =
+      process.env.REACT_APP_API_ACCESO_URL;
+    const datos = JSON.stringify({
+      nombre: usuario.name,
+      email: usuario.email,
+      aplicacion: "Admin"
     });
-    
-		Axios.post(
-			`${SERVICIO_DE_VERIFICACION_API_SERVICIO_DE_DATOS}/verificarAcceso`,
-			datos,
-			{
-				headers: {
-					"Content-Type": "application/json"
-				}
-			}
-		)
-			.then((respuesta) => {
-				setState({
-					validado: respuesta.data.data,
-					mostrarIconoCargando: false
-				});
-			})
-			.catch((error) => {
-				setState({
-					mostrarBotonDeCarga: false
-				});
-				alert("Hay un error en la base de datos, status: " + error.status);
-			});
-	};
 
-	function iconoDeCarga() {
-		return (
-			state.mostrarIconoCargando === true && (
-				<Dimmer active inverted>
-					<Loader>Verificando Acceso...</Loader>
-				</Dimmer>
-			)
-		);
-	}
-	return (
-		<div>
-			{iconoDeCarga()}
-			{state.validado ? <NahualTable /> : <h1>NO TIENES ACCESO</h1>}
-		</div>
-	);
+    Axios.post(
+      `${SERVICIO_DE_VERIFICACION_API_SERVICIO_DE_DATOS}/verificarAcceso`,
+      datos,
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .then(respuesta => {
+        setState({
+          validado: respuesta.data.data,
+          mostrarIconoCargando: false
+        });
+      })
+      .catch(error => {
+        setState({
+          mostrarBotonDeCarga: false
+        });
+        alert("Hay un error en la base de datos, status: " + error.status);
+      });
+  };
+
+  function iconoDeCarga() {
+    return (
+      state.mostrarIconoCargando === true && (
+        <Dimmer active inverted>
+          <Loader>Verificando Acceso...</Loader>
+        </Dimmer>
+      )
+    );
+  }
+  return (
+    <div>
+      {iconoDeCarga()}
+      {state.validado ? <NahualTable /> : <NoAutorizado />}
+    </div>
+  );
 };
 
 export default ValidarAutorizacion;
--- src/components/solicitudAcceso/tipo-mensaje/MensajeResultante.js
+++ src/components/solicitudAcceso/tipo-mensaje/MensajeResultante.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { Component } from 'react'
 import {Message} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
@@ -19,11 +20,39 @@ export class MensajeResultante extends Component {
             </div>
         )
     }
-}
+=======
+import React, { Component } from "react";
+import { Message } from "semantic-ui-react";
+import PropTypes from "prop-types";
+import "../../../public/stylesheets/Mensaje.css";
 
+export class MensajeResultante extends Component {
+  render() {
+    var colorDeMensaje = this.props.colorDeFondo;
+    return (
+      <div
+        style={{ border: "1px solid `${colorDeMensaje}` !important" }}
+        className="centerMessage"
+      >
+        <Message
+          color={`${colorDeMensaje}`}
+          size="big"
+          style={{
+            color: `${colorDeMensaje}`,
+            border: `${colorDeMensaje}`
+          }}
+        >
+          <Message.Header>{this.props.encabezadoDelMensaje}</Message.Header>
+          <p>{this.props.cuerpoDelMensaje}</p>
+        </Message>
+      </div>
+    );
+  }
+>>>>>>> 7b861ab... Se agrego la pagina de No acceso, y el boton de cerrar sesion
+}
 
 MensajeResultante.propTypes = {
-    todo: PropTypes.object.isRequired
-}
+  todo: PropTypes.object.isRequired
+};
 
-export default MensajeResultante
+export default MensajeResultante;
