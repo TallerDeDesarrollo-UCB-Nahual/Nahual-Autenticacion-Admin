e7e83f91d38cbce30d901edd451d73d26f5d1042
unknown <pablo.canelas.schlink@gmail.com>
2020-11-12 16:29:00 -0400

    Se añadieron mensajes de exito y falla al otorgar acceso

--- src/components/otorgar-acceso/FormAcceso.js
+++ src/components/otorgar-acceso/FormAcceso.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Button, Dropdown } from 'semantic-ui-react'
 import { Form, Input } from 'semantic-ui-react-form-validator'
 import axios  from 'axios';
+import MensajeResultante from '../solicitudAcceso/tipo-mensaje/MensajeResultante'
 
 class FormAcceso extends Component {
   constructor(props) {
@@ -35,6 +36,7 @@ class FormAcceso extends Component {
       .catch(error => {
         this.setState({ errorMessage: error.message });
         console.error('Hubo un error!', error);
+        this.setState({exito:false})
       });
   }
 
@@ -45,45 +47,52 @@ class FormAcceso extends Component {
       { key: 'Empresas', text: 'Empresas', value: 'Empresas' },
     ]
     return (
-      <Form id="myForm" onSubmit={this.enConfirmacion}>
-        <Input
-          name='nombre'
-          value={this.state.nombre}
-          validators={['required', 'matchRegexp:^[A-Za-z ]+$']}
-          errorMessages={['Este campo es requerido', 'El campo no acepta valores numéricos']}
-          id='form-acceso'
-          label='Nombre completo'
-          placeholder='Nombre completo'
-          width={16}
-          onChange={this.cambioEnInput}
-        />
-        <Input
-          name='correo'
-          value={this.state.correo}
-          type='email'
-          id='form-input-control-error-email'
-          label='Correo'
-          placeholder='ejemplo@****.com'
-          validators={['required']}
-          errorMessages={['Este campo es requerido']}
-          width={16}
-          onChange={this.cambioEnInput}
-        />
-        <p><b>Aplicación</b></p>
-        <Dropdown
-          compact
-          name='aplicacion'
-          placeholder='Aplicacion'
-          validators={['required']}
-          fluid
-          selection
-          options={opcionesAplicacion}
-          onChange={this.cambioEnInput}
-        />
-        <br></br>
-        <Button floated='right' color='red' onClick={this.props.cerrarModal}>Cancelar</Button>
-        <Button floated='right' type='submit' onSubmit={this.enConfirmacion}>Otorgar Acceso</Button>
-      </Form>
+      <div>
+         <Form id="myForm" onSubmit={this.enConfirmacion}>
+          <Input
+            name='nombre'
+            value={this.state.nombre}
+            validators={['required', 'matchRegexp:^[A-Za-z ]+$']}
+            errorMessages={['Este campo es requerido', 'El campo no acepta valores numéricos']}
+            id='form-acceso'
+            label='Nombre completo'
+            placeholder='Nombre completo'
+            width={16}
+            onChange={this.cambioEnInput}
+          />
+          <Input
+            name='correo'
+            value={this.state.correo}
+            type='email'
+            id='form-input-control-error-email'
+            label='Correo'
+            placeholder='ejemplo@****.com'
+            validators={['required']}
+            errorMessages={['Este campo es requerido']}
+            width={16}
+            onChange={this.cambioEnInput}
+          />
+          <p><b>Aplicación</b></p>
+          <Dropdown
+            compact
+            name='aplicacion'
+            placeholder='Aplicacion'
+            validators={['required']}
+            fluid
+            selection
+            options={opcionesAplicacion}
+            onChange={this.cambioEnInput}
+          />
+          <br></br>
+          <Button floated='right' color='red' onClick={this.props.cerrarModal}>Cancelar</Button>
+          <Button floated='right' type='submit' onSubmit={this.enConfirmacion}>Otorgar Acceso</Button>
+        </Form>
+        {(this.state.exito === true) && (
+        <MensajeResultante encabezadoDelMensaje= "Solicitud exitosa" cuerpoDelMensaje="Acceso otorgado con exito" colorDeFondo="green"/>)}
+        {(this.state.exito === false) && (
+        <MensajeResultante encabezadoDelMensaje= "Solicitud no exitosa" cuerpoDelMensaje="Hubo un error al momento de enviar, intente de nuevo más tarde" colorDeFondo="red"/>)}
+      </div>
+     
     )
   }
 }
